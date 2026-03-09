7a7f5566196ddc04a38824ab4c1f046ad7ced001
Oladimeji <boladimeji834@gmail.com>
2026-03-09 05:17:39 +0100

    finished the use effects for loading user sessions and messages

--- src/pages/customer/Chat.jsx
+++ src/pages/customer/Chat.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { getUserSessions, getMessages } from '../../api/chat'
+
+// code here runs before loading the component 
+
 
 export default function Chat () {
 
@@ -8,14 +13,55 @@ export default function Chat () {
     const [ loading, setLoading ] = useState(false)
     const [ sessions, setSessions ] = useState([])
     const [ input, setInput ] = useState("")
+    
+
+    useEffect(() => {
+    // useEffect cant be async 
+
+        async function fetchSessions () {
+            const sessions = await getUserSessions()
+            setSessions(sessions.data.sessions)
+        }
+        fetchSessions()
+    }, [])
+
+    useEffect(() => {
+        if (!activeSession) {
+            return
+        }
+        async function fetchSessionMessages(session) {
+            const messages = await getMessages(session)
+            setMessages(messages.data.messages)
+        }
+        fetchSessionMessages(activeSession)
+    }, [activeSession])
+
 
   return (
     <div className='flex flex-row'>
-        <div className='sidebar'>
+        <div className='' id='sidebar'>
+            <div>
+                <h1>ShopDesk</h1>
+            </div>
 
+            <div>
+                <button>New chat</button>
+            </div>
+
+            <div className='' id='session-list'>
+
+            </div>
         </div>
 
-        <div className='main'>
+
+        <div className='flex flex-col' id='main'>
+            <div className='' id='messages-area'>
+
+            </div>
+
+            <div id='submit-btn-area'>
+
+            </div>
 
         </div>
     </div>
